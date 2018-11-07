39fe2a011214cc414c6c96dac1d95629f425e9b4
Artur Morelle Nista <arturnista@gmail.com>
2018-11-07 21:19:57 -0200

    Fixes

--- Assets/FinalScreenController/FinalScreenController.cs
+++ Assets/FinalScreenController/FinalScreenController.cs
@@ -7,6 +7,6 @@ public class FinalScreenController : MonoBehaviour {
 
 	public void Restart() {
         GameController.main.Restart();
-        SceneManager.LoadScene("Rooms/Room_01/Scene", LoadSceneMode.Single);		
+        SceneManager.LoadScene("Rooms/Room_00/Scene", LoadSceneMode.Single);		
 	}
 }
